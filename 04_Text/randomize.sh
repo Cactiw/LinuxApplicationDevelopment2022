5e689324d978493b9b06aa1cdea4656a78f7ce71
cactiw <Knyazev1514@gmail.com>
2022-10-15 22:36:00 +0300

    Removed comments

--- 04_Text/randomize.sh
+++ 04_Text/randomize.sh
@@ -16,25 +16,17 @@ do
 done
 
 last_line=y
-#echo "${data[@]}"
 tput clear
-#echo "${data[@]}"
 data_shuffled="$(echo "${data[@]}" | xargs -n1 | sort -R)"
-#data_shuffled="$data_shuffled"
-#echo "$data_shuffled"
-#echo "${data[@]}" | sort -R | cat
 
 while IFS='.' read -r cur_x cur_y char
 do
-#  echo "$line"
-#  echo $cur_x "." $cur_y "." $char
   tput cup "$cur_y" "$cur_x"
   echo -n "$char"
   if [[ ${char: -1} != $'\r' ]] && [[ $char != $' ' ]]; then
+    # shellcheck disable=SC2086
     sleep $SLEEP_DELAY
     fi
 
 done <<< "$data_shuffled"
-#done < <(printf %s "$data_shuffled")
 tput cup $last_line 0
-
