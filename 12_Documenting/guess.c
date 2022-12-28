1933f620bb2c9f9cdcc1b204f92eb7b942c4cbfa
Cactiw <Knyazev1514@gmail.com>
2022-12-28 16:35:32 +0300

    Added roman system

--- 12_Documenting/guess.c
+++ 12_Documenting/guess.c
@@ -52,10 +52,96 @@ int roman_to_int (char *s)
     return int_num;
 }
 
-int main() {
+char* int_to_roman(int num)
+{
+    char *roman = malloc(16);
+    int i = 0;
+
+    while (num >= 1000) {
+        roman[i++] = 'M';
+        num -= 1000;
+    }
+    while (num >= 900) {
+        roman[i++] = 'C';
+        roman[i++] = 'M';
+        num -= 900;
+    }
+    while (num >= 500) {
+        roman[i++] = 'D';
+        num -= 500;
+    }
+    while (num >= 400) {
+        roman[i++] = 'C';
+        roman[i++] = 'D';
+        num -= 400;
+    }
+    while (num >= 100) {
+        roman[i++] = 'C';
+        num -= 100;
+    }
+    while (num >= 90) {
+        roman[i++] = 'X';
+        roman[i++] = 'C';
+        num -= 90;
+    }
+    while (num >= 50) {
+        roman[i++] = 'L';
+        num -= 50;
+    }
+    while (num >= 40) {
+        roman[i++] = 'X';
+        roman[i++] = 'L';
+        num -= 40;
+    }
+    while (num >= 10) {
+        roman[i++] = 'X';
+        num -= 10;
+    }
+    while (num >= 9) {
+        roman[i++] = 'I';
+        roman[i++] = 'X';
+        num -= 9;
+    }
+    while (num >= 5) {
+        roman[i++] = 'V';
+        num -= 5;
+    }
+    while (num >= 4) {
+        roman[i++] = 'I';
+        roman[i++] = 'V';
+        num -= 4;
+    }
+    while (num >= 1) {
+        roman[i++] = 'I';
+        num -= 1;
+    }
+    roman[i] = '\0';
+    return roman;
+}
+
+char* get_number(int n, int use_roman) {
+    char *number = malloc(16);
+    if (use_roman) {
+        number = int_to_roman(n);
+    } else {
+        sprintf(number, "%d", n);
+    }
+    return number;
+}
+
+int main(int argc, char **argv) {
     bindtextdomain("guess", getenv("PWD"));
     setlocale(LC_ALL, "");
     textdomain("guess");
+
+    int use_roman = 0;
+    if (argc > 1) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf(_("Usage: %s [number]"));
+        } else if (strcmp(argv[1], "-r") == 0) {
+            use_roman = 1;
+        }
+}
 
     char* answer = NULL;
     int guess;
@@ -63,7 +149,7 @@ int main() {
     int l_border = MIN, r_border = MAX;
     while (r_border - 1 > l_border) {
         guess = (l_border + r_border) / 2;
-        printf(_("Is your number greater than %d? (y/n) "), guess);
+        printf(_("Is your number greater than %s? (y/n) "), get_number(guess, use_roman));
         while (1) {
             scanf("%ms", &answer);
             if (strcmp(answer, _("y")) == 0) {
@@ -78,5 +164,10 @@ int main() {
             }
         }
     }
-    printf(_("Your number is %d\n"), r_border);
+
+    if (use_roman) {
+        printf(_("Your number is %s\n"), int_to_roman(r_border));
+    } else {
+        printf(_("Your number is %d\n"), r_border);
+    }
 }
